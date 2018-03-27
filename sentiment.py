49f787ea88d67651101b167e7139c23b0003a7ee
jgreelyBR <jason.greely@bloomreach.com>
2018-03-27 16:08:55 -0400

    oops

--- sentiment.py
+++ sentiment.py
@@ -120,7 +120,7 @@ def process_subreddit(subreddit, subreddit_name, path):
                 "negative_comments" : neg
             })
 
-            print("Success: CSV created in {}".format(file_path))
+        print("Success: CSV created in {}".format(file_path))
 
 
 def clean_comment(comment):
